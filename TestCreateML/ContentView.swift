2903156604e627ccdd7afc82cf07b95195a23356
Anmol Singh <anmolsingh1597@gmail.com>
2020-07-28 16:37:07 -0400

    Initial Commit

---
+++ TestCreateML/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestCreateML
+//
+//  Created by Anmol singh on 2020-07-28.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
